5827722c7f3b7b1e1774d867450802b5cecb3e0a
Anthony Lukach <anthonylukach@gmail.com>
2022-10-15 22:15:36 -0700

    Mantine basic functionality

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,5 +1,23 @@
-import { prisma } from "~/db.server";
+// import { prisma } from "~/db.server";
 
-export async function getPosts() {
-  return prisma.post.findMany();
+// export async function getPosts() {
+//   return prisma.post.findMany();
+// }
+
+type Post = {
+  slug: string;
+  title: string;
+};
+
+export async function getPosts(): Promise<Array<Post>> {
+  return [
+    {
+      slug: "my-first-post",
+      title: "My First Post",
+    },
+    {
+      slug: "90s-mixtape",
+      title: "A Mixtape I Made Just For You",
+    },
+  ];
 }
